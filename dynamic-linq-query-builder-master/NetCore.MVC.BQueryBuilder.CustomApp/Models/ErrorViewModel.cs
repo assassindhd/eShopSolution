b0e2105967e8c3fba6ecfb3d374a9fc189739087
Pham Phu Duc <ducpp@hup.edu.vn>
2024-05-04 16:47:11 +0700

    Add Project NetCore Razor Page và NetCore MVC, So sánh sự khác nhau, Razor Page cấu trúc giống ASPX Page

---
+++ dynamic-linq-query-builder-master/NetCore.MVC.BQueryBuilder.CustomApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCore.MVC.BQueryBuilder.CustomApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
