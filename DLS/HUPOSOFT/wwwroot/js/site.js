15d397c9e995ca862a3577fa8e7e712a02edf96e
Pham Phu Duc <ducpp@hup.edu.vn>
2024-05-07 16:51:15 +0700

    thêm CSS, JS cho HUPOSOFT

--- DLS/HUPOSOFT/wwwroot/js/site.js
+++ DLS/HUPOSOFT/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function js_selectOnchange(value, txtName) {
+	//console.log('value', value);
+	//console.log('Destination', txtName);
+	var result = '';
+	for (var i = 0; i < value.selectedOptions.length; i++) {
+		result += value.selectedOptions[i].value + '#';
+	};
+	console.log('result', result);
+	var txt = document.getElementById(txtName);
+	//let txt = $('#' + txtName);
+	txt.value = result;
+};
+
+
+function js_radio_handleClick(myRadio, TextBoxResult) {
+	//alert('TextBox Result: ' + TextBoxResult);
+	var txt = document.getElementById(TextBoxResult);
+	//let txt = $('#' + txtName);
+	txt.value = myRadio.value;
+};
